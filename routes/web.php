c9b5b5cad226ee57b037ef9b39e7778918d15bf2
SHoangDang <danghoang2861997@gmail.com>
2018-11-06 17:07:40 +0700

    Add space

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,5 @@ Route::group(['prefix' => 'admin'], function () {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
 });
