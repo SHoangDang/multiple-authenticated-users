0b9ccc8c6bcd8b94e7bb9048c62f48d526b35185
SHoangDang <danghoang2861997@gmail.com>
2018-11-10 00:48:36 +0700

    Forgot and Reset password fixes

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,10 @@ Route::group(['prefix' => 'admin'], function () {
 
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.reset');
+    Route::post('password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+    Route::post('password/reset', 'Auth\AdminResetPasswordController@reset')->name('admin.password.update');
+
 });
